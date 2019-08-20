15a373513e2083bbf254c9e8ff6cf066a7efeb40
Pulkit Gupta <guppul@gmail.com>
2019-08-20 13:47:40 +0530

    fix

--- index.js
+++ index.js
@@ -3934,7 +3934,7 @@ ace.define("ace/mouse/touch_handler",["require","exports","module","ace/mouse/mo
         });
 
         function animate() {
-            animationSteps += 40;
+            animationSteps += 60;
             animationTimer = setInterval(function() {
                 if (animationSteps-- <= 0) {
                     clearInterval(animationTimer);
