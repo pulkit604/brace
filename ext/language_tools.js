e8a9e5ae285d8bce9d20f92bcc9326578594c38c
Pulkit Gupta <guppul@gmail.com>
2019-09-23 18:02:08 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1447,10 +1447,10 @@ var Autocomplete = function() {
                 this.editor.find('codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
                 this.editor.session.selection.clearSelection();
                 this.position = this.editor.getCursorPosition();
-                const row = this.position.row;
-                const col = this.position.column;
-                this.editor.session.addFold('', new Range(row, col, row, col + 19));
-                this.editor.session.addFold('', new Range(row, col + data.snippet.length + 19, row, col + data.snippet.length+ 36));
+                var curr_row = this.position.row;
+                var curr_col = this.position.column;
+                this.editor.session.addFold('', new Range(curr_row, curr_col, curr_row, curr_col + 19));
+                this.editor.session.addFold('', new Range(curr_row, curr_col + data.snippet.length + 19, curr_row, curr_col + data.snippet.length+ 36));
             }
             else
                 this.editor.execCommand("insertstring", data.value || data);
