559b49f0d5455e082a301a5f9bf4c75ab7126777
Pulkit Gupta <guppul@gmail.com>
2019-09-23 18:15:31 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1449,6 +1449,7 @@ var Autocomplete = function() {
                 var position = this.editor.getCursorPosition();
                 var curr_row = position.row;
                 var curr_col = position.column;
+                console.log(new Range(curr_row, curr_col, curr_row, curr_col + 19), ''));
                 this.editor.session.addFold(new Range(curr_row, curr_col, curr_row, curr_col + 19), '');
                 this.editor.session.addFold(new Range(curr_row, curr_col + data.snippet.length + 19, curr_row, curr_col + data.snippet.length+ 36), '');
             }
