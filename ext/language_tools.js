28d342934cc92024530e8a5a0f502778b91b18c3
Pulkit Gupta <guppul@gmail.com>
2019-09-24 18:02:31 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1447,8 +1447,7 @@ var Autocomplete = function() {
                 snippetManager.insertSnippet(this.editor, 'codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
                 this.editor.find('codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
                 this.editor.session.selection.selectWordLeft();
-                console.log(this.editor.selection.getAllRanges());
-                var position = this.editor.selection.getRange();
+                var position = this.editor.selection.getWordRange();
                 var curr_row = position.start.row;
                 var curr_col = position.start.column;
                 this.editor.session.addFold('', new Range(curr_row, curr_col, curr_row, curr_col + 19));
