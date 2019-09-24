857264eac1742eb40f67259b30854c65175e4415
Pulkit Gupta <guppul@gmail.com>
2019-09-24 14:20:15 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1446,11 +1446,11 @@ var Autocomplete = function() {
             if (data.snippet) {
                 snippetManager.insertSnippet(this.editor, 'codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
                 this.editor.find('codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
-                this.editor.session.selection.clearSelection();
+                //this.editor.session.selection.clearSelection();
                 var position = this.editor.getCursorPosition();
                 var curr_row = position.row;
                 var curr_col = position.column;
-                //this.editor.session.addFold('', new Range(curr_row, curr_col, curr_row, curr_col + 19));
+                this.editor.session.addFold('', new Range(curr_row, curr_col, curr_row, curr_col + 19));
                 this.editor.session.addFold('', new Range(curr_row, curr_col + data.snippet.length + 19, curr_row, curr_col + data.snippet.length+ 36));
             }
             else
