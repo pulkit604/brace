71a93e07ddb9ad37935cd2136afbff0fac1cada6
Pulkit Gupta <guppul@gmail.com>
2019-09-24 16:41:12 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1446,7 +1446,8 @@ var Autocomplete = function() {
             if (data.snippet) {
                 snippetManager.insertSnippet(this.editor, 'codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
                 this.editor.find('codepuzzleoption_A_' + data.snippet + '_codepuzzleoption');
-                var position = this.editor.selection.getCursorPosition();
+                var position = this.editor.selection.getCursor();
+                console.log(position);
                 this.editor.session.selection.clearSelection();
                 var curr_row = position.row;
                 var curr_col = position.column;
