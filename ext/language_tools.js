6c6b45ec81eed378f21f8311aed32a43e39d9580
Pulkit Gupta <guppul@gmail.com>
2019-10-10 17:13:17 +0530

    fix

--- ext/language_tools.js
+++ ext/language_tools.js
@@ -1389,7 +1389,7 @@ var Autocomplete = function() {
     this.changeListener = function(e) {
         var cursor = this.editor.selection.lead;
         if (cursor.row != this.base.row || cursor.column < this.base.column) {
-            console.log(cursor);
+            debugger;
             this.detach(true);
         }
         if (this.activated)
