0682ec385c0de9e3ebcadb19ad6b7a3f178551ca
Pulkit Gupta <guppul@gmail.com>
2019-11-07 10:08:39 +0530

    fix

--- index.js
+++ index.js
@@ -2521,15 +2521,10 @@ ace.define("ace/keyboard/textinput",["require","exports","module","ace/lib/event
                 afterContextMenu = false;
         };
         var onInput = function(e) {
+            debugger;
             if (inComposition)
                 return;
             var data = text.value;
-            for (let i=0;i<data.length;i++) {
-                if (data[i].charCodeAt(0) >= 5000) {
-                    console.log(data[i].charCodeAt(0));
-                    return;
-                }
-            }
             sendText(data);
             resetValue();
         };
