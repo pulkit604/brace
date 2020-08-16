785dd54fbac9a63ede0b05ca383c399965635ff3
Pulkit Gupta <guppul@gmail.com>
2020-08-16 22:41:08 +0200

    ignore for xml

--- mode/html.js
+++ mode/html.js
@@ -481,7 +481,7 @@ function JSX() {
         regex : "="
     }, {
         token : "text.tag-whitespace.xml",
-        regex : "\\s+(?![__A__])"
+        regex : "\\s(?![__A__])+"
     }, {
         token : "string.attribute-value.xml",
         regex : "'",
